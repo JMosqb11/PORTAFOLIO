feca966733b3f048700989c4be169ac2471c323b
Jhonier Mosquera <jhoniermosquera61@gmail.com>
2022-10-28 20:30:27 -0500

    portafolio

---
+++ index.js
@@ -0,0 +1,47 @@
+const correo = document.querySelector('#email');
+const enviar = document.querySelector('#btn');
+
+// enviar.addEventListener('click', () => {
+
+//     function validacion(correo) {
+
+//         re=/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$/
+    
+//         if (!re.exec(correo)) {
+    
+//             enviar.addEventListener('click', () => {
+    
+//                 alert('El campo debe tener un correo electronico');
+    
+//             });
+          
+//         }else{
+    
+           
+//         }
+    
+//     }
+// });
+
+
+// function validacion(correo) {
+
+//     re=/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$/
+
+//     if (!re.exec(correo)) {
+
+//         enviar.addEventListener('click', () => {
+
+//             alert('El campo debe tener un correo electronico');
+
+//         });
+      
+//     }else{
+
+//         enviar.addEventListener('click', () => {
+            
+//         });
+//     }
+
+// }
+
